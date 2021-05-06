ad3643741ccb25c10eb205e88330d3c60c54881f
LAPTOP-1BL52TMD\kasia <kasiakadziolka@gmail.com>
2021-05-06 21:00:35 +0200

    start project

--- Portfolio/Portfolio/Program.cs
+++ Portfolio/Portfolio/Program.cs
@@ -1,25 +1,28 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Portfolio
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+namespace Portfolio {
+	public class Program {
+		public static async Task Main(string[] args) {
+			var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddSingleton(new HttpClient {
+				BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+			});
 
-            await builder.Build().RunAsync();
-        }
-    }
+			builder.RootComponents.Add<App>("#app");
+			
+			var host = builder.Build();
+
+
+			await host.RunAsync();
+		}
+	}
 }
