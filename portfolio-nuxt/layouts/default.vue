faea5df73edc3b2ae19b345a66f8e1ae4c4b658b
Katarzyna <kasiakadziolka@gmail.com>
2023-09-27 13:56:18 +0200

    navbar title

--- portfolio-nuxt/layouts/default.vue
+++ portfolio-nuxt/layouts/default.vue
@@ -3,8 +3,8 @@
 
 <template>
   <div class="default_container">
-    Some default layout shared across all pages
-
+    <img src="../assets/logo.png" alt="logo" class="default_logo">
+    <div class="default_title">Katarzyna Kądziołka</div>
   </div>
   <slot />
 </template>
@@ -12,8 +12,21 @@
 <style scoped lang="scss">
 .default {
   &_container {
-    background: linear-gradient(90deg, $primary, $gradient);
-    height: 56px;
+    background: linear-gradient(90deg, $primary, $p-secondary);
+    height: 40px;
+    border-bottom: $p-secondary solid 1px;
+    display: flex;
+    align-items: center;
+    padding: 8px;
+    column-gap: 8px;
+  }
+  &_logo {
+    width: 40px;
+    height: 40px;
+  }
+  &_title {
+    color: white;
+    font-size: 1.5rem;
   }
 }
 </style>
