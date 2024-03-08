19e0e0a474b178c121f00a8cb1ecb6ae5f65a3f7
Katarzyna <kasiakadziolka@gmail.com>
2024-03-08 14:07:47 +0100

    Certificate list

---
+++ portfolio-nuxt/components/features/certificates/ListItem.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+
+defineProps<{
+  name: string,
+  organization: string,
+  active: boolean
+}>()
+
+</script>
+
+<template>
+  <div class="list-item_container" :class="{'list-item_active': active}" @click="$emit('changeActiveCertificate', name)">
+    <span class="list-item_name">{{name}}</span>
+    <span class="list-item_organization">{{organization}}</span>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.list-item {
+  &_container {
+    border-bottom: 2px solid $primary;
+    display: flex;
+    flex-direction: column;
+    padding: 8px;
+    cursor: pointer;
+    &:hover {
+      background: $nav-primary;
+    }
+  }
+  &_name {
+    font-size: 1.5rem;
+  }
+  &_organization {
+  }
+  &_active {
+    background: $nav-primary;
+  }
+}
+</style>
--- portfolio-nuxt/pages/Certificates.vue
+++ portfolio-nuxt/pages/Certificates.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
+import {certificates} from "~/data/certificates"
+import ListItem from "~/components/features/certificates/ListItem.vue";
+
+const activeCertificateName = ref(certificates[0].Name)
+const onChangeActiveCertificate = (activeName: string) => {
+  activeCertificateName.value = activeName
+}
 
 </script>
 
 <template>
   <div class="certificates_container">
     <div>
-      Certificates
+      <ListItem v-for="certificate in certificates" :key="certificate" :name="certificate.Name" :organization="certificate.Organization" :active="certificate.Name === activeCertificateName" @changeActiveCertificate="onChangeActiveCertificate"/>
     </div>
     <div>
       Single certificate
@@ -18,8 +25,9 @@
   &_container {
     display: grid;
     grid-template-columns: 1fr 1fr;
-    padding: 64px 24px 24px;
+    padding: 32px 24px 24px;
     max-width: 100vw;
+    column-gap: 32px;
     @media (max-width: 768px) {
       grid-template-columns: 1fr;
       grid-template-rows: 1fr 1fr;
