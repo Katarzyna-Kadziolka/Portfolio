6b62298c39f61fb111b8cf15311f476e95672267
Katarzyna <kasiakadziolka@gmail.com>
2024-03-08 09:44:45 +0100

    Add filter bar

--- portfolio-nuxt/pages/Certificates.vue
+++ portfolio-nuxt/pages/Certificates.vue
@@ -1,5 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Filter from "~/components/common/Filter.vue";
+import {TechnologyFilter} from "~/types/technologyFilter";
+import {Technology} from "~/types/technology";
+
+const filters: TechnologyFilter[] = reactive([
+  {
+    Technology: Technology.Csharp,
+    IsChecked: false
+  },
+  {
+    Technology: Technology.Vue,
+    IsChecked: false
+  },
+]);
+</script>
 
 <template>
+  <Filter :filters="filters"/>
   Certificates
 </template>
