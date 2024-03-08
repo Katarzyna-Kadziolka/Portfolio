f210dec598834b83009ae9fa37224ae83968e485
Katarzyna <kasiakadziolka@gmail.com>
2024-03-08 10:00:08 +0100

    main skeleton

--- portfolio-nuxt/pages/Certificates.vue
+++ portfolio-nuxt/pages/Certificates.vue
@@ -17,5 +17,27 @@ const filters: TechnologyFilter[] = reactive([
 
 <template>
   <Filter :filters="filters"/>
-  Certificates
+  <div class="certificates_container">
+    <div>
+      Certificates
+    </div>
+    <div>
+      Single certificate
+    </div>
+  </div>
 </template>
+
+<style scoped lang="scss">
+.certificates {
+  &_container {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    padding: 64px 24px 24px;
+    max-width: 100vw;
+    @media (max-width: 768px) {
+      grid-template-columns: 1fr;
+      grid-template-rows: 1fr 1fr;
+    }
+  }
+}
+</style>
