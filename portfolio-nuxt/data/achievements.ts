65f0dfc316da550d4a6ccfa05786052d158d48d8
Katarzyna <kasiakadziolka@gmail.com>
2024-03-12 10:47:52 +0100

    images

--- portfolio-nuxt/data/achievements.ts
+++ portfolio-nuxt/data/achievements.ts
@@ -1,34 +1,36 @@
 import {Achievement} from "~/types/achievement";
-
+import aquathon from "~/assets/images/aquathon.png"
+import hackyeah from "~/assets/images/hachyeah.png"
+import sheep from "~/assets/images/sheep.png"
 export const achievements: Achievement[] = [
     {
         Name: "Double finalist at Hackyeah 2023",
         Date: "09.2023",
         Description: "Together with my team 'Śpiące Wydry', we made it through to the finals in TWO categories at the HackYeah 2023 hackathon: Paneuropa and HackSQL: Kolobrzeg - Future Competence Campus",
-        Image: ""
+        Image: hackyeah
     },
     {
         Name: "Finalist at second edition of Aquathon",
         Date: "06.2022",
         Description: "Once again, as 'Śpiące Wydry', we took part in the second edition of the Aquathon, this time making it to the finals. We developed the Municipal Well application, which streamlined the management of water by the municipal board in emergency situations. I was responsible for the web application for residents, which provided information functions and allowed residents to report incidents.",
-        Image:""
+        Image: aquathon
     },
     {
         Name: "Special award place at SheepYourHack 3",
         Date: "12.2021",
         Description: "As 'Śpiące Wydry', we received a distinction. Hurray! We have created an app that helps solve the problem of sharing urban spaces with wild animals. We want to raise public awareness to make metropolises nature-friendly, so that we and the animals can feel at home in them. I was co-responsible for writing the API using ASP.Net Core and C#.",
-        Image: ""
+        Image: sheep
     },
     {
         Name: "2nd place in Aquathon 2021",
         Date: "11.2021",
         Description: "Together with my team 'Śpiące Wydry', we took second place. We created an app called Raindrop for setting up and managing backyard rainwater tanks. I was responsible for creating a website using Vue.js.",
-        Image: ""
+        Image: aquathon
     },
     {
         Name: "1st place in the SheepYourHack 2",
         Date: "11.2020",
         Description: "My team 'Śpiące Wydry' won first place in the SheepYourHack 2 hackathon. I was responsible for project management, presentation and business development. Our project was an app called Printy. It solves the problem of poor access to print outlets. Using the app, anyone can find a printer from an individual close to them and print something. The printer, on the other hand, can make money from lending his unused printer. The app also supports 3D printing and implies cooperation not only with individuals, but also with printers.",
-        Image: ""
+        Image: sheep
     }
 ]
